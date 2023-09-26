b2ecd077dcfa8dcb30bbb33410ef997884ef0da9
d.marillanca01 <d.marillanca01@ufromail.cl>
2023-09-26 01:27:18 -0300

    se extrae funcion sendResponse de controladores en utils

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,55 +1,40 @@
-// src/controllers/gameController.js
-import { getGameByName, getRandomGamesForConsole, getRandomGamesByGenre, getRandomGameByGenreAndConsole } from '../services/gameService.js';
+// gameController.js
+import {
+  getGameByName,
+  getRandomGamesForConsole,
+  getRandomGamesByGenre,
+  getRandomGameByGenreAndConsole,
+} from '../services/gameService.js';
+
+import { sendResponse } from '../utils/controllerUtils.js'; // Importamos sendResponse desde el directorio 'utils'
 
 // Función para buscar un juego por nombre
 const findGameByName = (req, res) => {
   const { name } = req.query;
   const game = getGameByName(name);
-
-  if (!game) {
-    return res.status(404).json({ error: 'Juego no encontrado.' });
-  }
-
-  res.json(game);
+  sendResponse(res, game);
 };
 
 // Función para recomendar juegos aleatorios para una consola específica
 const recommendRandomGamesForConsole = (req, res) => {
   const { consoleName } = req.params;
   const recommendedGames = getRandomGamesForConsole(consoleName, 3);
-
-  if (recommendedGames.length === 0) {
-    return res.status(404).json({ error: 'No se encontraron juegos para la consola especificada.' });
-  }
-
-  res.json(recommendedGames);
+  sendResponse(res, recommendedGames);
 };
 
-
+// Función para obtener un juego aleatorio por género y consola
 const randomGameByGenreAndConsole = (req, res) => {
   const { console_abreviation } = req.params;
   const { genre_name } = req.body;
-
-  const result = getRandomGameByGenreAndConsole(genre_name, console_abreviation);
-
-  if (result.length === 0) {
-    res.status(404).json({ error: "No games found for specified console and genre" });
-  } else {
-    res.status(200).json(result);
-  }
+  const gamesForConsole = getRandomGameByGenreAndConsole(genre_name, console_abreviation);
+  sendResponse(res, gamesForConsole);
 };
-
 
 // Función para recomendar juegos aleatorios para un género específico
 const recommendRandomGamesByGenre = (req, res) => {
-  const { genreName } = req.body;
-  const recommendedGames = getRandomGamesByGenre(genreName, 3);
-
-  if (recommendedGames.length === 0) {
-    return res.status(404).json({ error: 'No se encontraron juegos para el género especificado.' });
-  }
-
-  res.json(recommendedGames);
+  const { genre_name } = req.body;
+  const recommendedGames = getRandomGamesByGenre(genre_name, 3);
+  sendResponse(res, recommendedGames);
 };
 
 export { findGameByName, recommendRandomGamesForConsole, recommendRandomGamesByGenre, randomGameByGenreAndConsole };
